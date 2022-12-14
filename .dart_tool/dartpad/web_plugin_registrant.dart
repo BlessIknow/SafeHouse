ab83c14c734ae85a6bae34fbe07ec7a97f1a7af6
lo-boy <35974208+lo-boy@users.noreply.github.com>
2022-12-14 23:50:14 +0300

    updated

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -7,14 +7,10 @@
 // ignore_for_file: type=lint
 
 import 'package:image_picker_web/image_picker_web.dart';
-import 'package:video_player_web/video_player_web.dart';
-import 'package:wakelock_web/wakelock_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   ImagePickerWeb.registerWith(registrar);
-  VideoPlayerPlugin.registerWith(registrar);
-  WakelockWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }
