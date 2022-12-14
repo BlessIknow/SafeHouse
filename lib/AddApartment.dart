b98a887522e751c207a36949204603668644fb3c
lo-boy <35974208+lo-boy@users.noreply.github.com>
2022-12-14 23:44:00 +0300

    #8

--- lib/AddApartment.dart
+++ lib/AddApartment.dart
@@ -1,104 +1,116 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-// import 'package:image_picker_web/image_picker_web.dart';
-// import 'dart:typed_data';
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker_web/image_picker_web.dart';
+import 'dart:typed_data';
+import 'UserProfile.dart';
 
 // void main() {
-//   runApp(MyApp());
+//   runApp(AddAppartment());
 // }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class AddAppartment extends StatelessWidget {
+  const AddAppartment({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: AddApartment(),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: AddApartment(),
+    );
+  }
+}
 
-// class AddApartment extends StatefulWidget {
-//   @override
-//   State<AddApartment> createState() => _AddApartment();
-// }
+class AddApartment extends StatefulWidget {
+  @override
+  State<AddApartment> createState() => _AddApartment();
+}
 
-// class _AddApartment extends State<AddApartment> {
-//   bool imageAvailable = false;
+class _AddApartment extends State<AddApartment> {
+  bool imageAvailable = false;
 
-//   late Uint8List imageFile;
+  late Uint8List imageFile;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Add Apartment"),
-//       ),
-//       body: SingleChildScrollView(
-//         child: Padding(
-//           padding: const EdgeInsets.all(16.0),
-//           child: Column(
-//             children: [
-//               TextField(
-//                 decoration: InputDecoration(
-//                   labelText: "House Address",
-//                 ),
-//               ),
-//               TextField(
-//                 decoration: InputDecoration(
-//                   labelText: "Apartment Type",
-//                 ),
-//               ),
-//               TextField(
-//                 decoration: InputDecoration(
-//                   labelText: "Description",
-//                 ),
-//               ),
-//               TextField(
-//                 decoration: InputDecoration(
-//                   labelText: "City",
-//                 ),
-//               ),
-//               TextField(
-//                 decoration: InputDecoration(
-//                   labelText: "Country",
-//                 ),
-//               ),
-//               SizedBox(height: 20),
-//               Container(
-//                   height: 200,
-//                   width: 200,
-//                   color: Colors.white,
-//                   child: imageAvailable
-//                       ? Image.memory(imageFile)
-//                       : const SizedBox()),
-//               SizedBox(height: 20),
-//               GestureDetector(
-//                 onTap: () async {
-//                   final image = await ImagePickerWeb.getImageAsBytes();
-//                   setState(() {
-//                     imageFile = image!;
-//                     imageAvailable = true;
-//                   });
-//                 },
-//                 child: Container(
-//                   height: 50,
-//                   width: 150,
-//                   child: Center(
-//                     child: Text('Upload Apartment Image'),
-//                   ),
-//                 ),
-//               ),
-//               SizedBox(height: 20),
-//               MaterialButton(
-//                 onPressed: (() {}),
-//                 child: Text("Submit"),
-//                 color: Colors.blue,
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Apartment"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(150, 20, 150, 20),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Container(
+                  height: 200,
+                  width: 200,
+                  color: Colors.grey,
+                  child: imageAvailable
+                      ? Image.memory(imageFile)
+                      : const SizedBox()),
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: () async {
+                  final image = await ImagePickerWeb.getImageAsBytes();
+                  setState(() {
+                    imageFile = image!;
+                    imageAvailable = true;
+                  });
+                },
+                child: Container(
+                  height: 50,
+                  width: 150,
+                  child: Center(
+                    child: Text('Click here Upload Apartment Image'),
+                  ),
+                ),
+              ),
+              Center(
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "House Address",
+                  ),
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Apartment Type",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Description",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "City",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Country",
+                ),
+              ),
+              SizedBox(height: 20),
+              MaterialButton(
+                onPressed: (() {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Userprofile()));
+                }),
+                child: Text("Submit"),
+                color: Colors.teal,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
